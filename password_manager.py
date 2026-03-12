80d422045334838c935dc6fe8c83999f2fd51413
peter karanja <karanjapitah1@gmail.com>
2026-03-12 12:24:52 +0300

    Upgrade password manager with generator, search, counter, and logging

--- password_manager.py
+++ password_manager.py
@@ -1,3 +1,5 @@
+import random
+import string
 def load_passwords():
     
     passwords = {}
@@ -43,36 +45,78 @@ def count_passwords(passwords):
 
     print(f"You have {len(passwords)} stored passwords")
 
+def generate_password():
+    length = int(input("Enter the length of the password: "))
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''
+
+    for _ in range(length):
+        password += random.choice(characters)
+    print(f"Generated Password: {password}")
+
+def search_service(passwords):
+    keyword = input("Enter the service name to search for: ")
+
+    for account in passwords:
+        if keyword.lower() in account.lower():
+            print(account)
+def log_action(action):
+    with open("log.txt", "a") as file:
+        file.write(f"{action}\n")
+
+
 passwords = load_passwords()
 
 
 while True:
-    print("\n1 Add password")
-    print("2 Get password")
-    print("3 Delete password")
-    print("4 Show services")
-    print("5 count passwords")
-    print("6 Exit")
+    print("""
+MENU
+1 Add password
+2 Get password
+3 Delete password
+4 Show services
+5 Count passwords
+6 Generate password
+7 Search service
+8 Exit
+""")
+    
 
     choice = (input("\nEnter your choice: "))
     if choice == "1":
         add_password(passwords)
         save_passwords(passwords)
+        log_action("Password added")
+
     elif choice == "2":
         get_password(passwords)
+        log_action("Password retrieved")
         
     elif choice == "3":
         delete_password(passwords)
         save_passwords(passwords)
+        log_action("Password deleted")
+
     elif choice == "4":
         show_all_services(passwords)
+        log_action("Services shown")
         
         
     elif choice == "5":
         count_passwords(passwords)
-        
+        log_action("Password count")
+
     elif choice == "6":
+        generate_password()
+        log_action("Password generated")
+
+    elif choice == "7":
+        search_service(passwords)
+        log_action("Service searched")
+        
+    elif choice == "8":
         print("Exiting the program.")
+        log_action("Program exited")
         break
        
     else:
